671ba22fe12a311cc97517728c7acbbd6cc0e3c9
dfpj <pysedev@gmail.com>
2021-07-27 14:50:33 +0430

    end course 17 add persian number

--- extensions/utils.py
+++ extensions/utils.py
@@ -1,20 +1,29 @@
 from . import jalali
 from django.utils import timezone
 
+def persian_numbers_converter(numstr):
+    numbers={
+        "0":"۰",
+        "1":"۱",
+        "2":"۲",
+        "3":"۳",
+        "4":"۴",
+        "5":"۵",
+        "6":"۶",
+        "7":"۷",
+        "8":"۸",
+        "9":"۹",
+    }
+    for e,p in numbers.items():
+        numstr=numstr.replace(e,p)
+    return numstr
+
 def jalali_converter(time):
     jmonths=["فروردین","اردیبهشت","خرداد","تیر","مرداد","شهریور","مهر","آبان","آذر","دی","بهمن","اسفند"]
-    
     time=timezone.localtime(time)
     time_to_str="{},{},{}".format(time.year,time.month,time.day)
     time_to_tuple=jalali.Gregorian(time_to_str).persian_tuple()
-
-
-    output ="{} {} {} {}:{} ".format(
-        time_to_tuple[2],
-        jmonths[time_to_tuple[1]-1],
-        
-        time_to_tuple[0],
-        time.hour,
-        time.minute
-    )
-    return output
+    day=persian_numbers_converter(str(time_to_tuple[0]))
+    month=jmonths[time_to_tuple[1]-1]
+    year=persian_numbers_converter(str(time_to_tuple[2]))
+    return f"زمان : {year}/{month}/{day}"
